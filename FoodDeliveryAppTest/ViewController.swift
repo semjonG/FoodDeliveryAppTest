2515ef01736ce657df6385c3501c908d6db5c08f
SemjonG <simon.gerasimov@gmail.com>
2022-02-13 21:19:53 +0300

    Initial Commit

---
+++ FoodDeliveryAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryAppTest
+//
+//  Created by mac on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
